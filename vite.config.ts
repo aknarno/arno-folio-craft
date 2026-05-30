8b39cd20a2e189fd3624dab4b6ca4cd29b16f59a
aknarno <arnoklettenbergneto@gmail.com>
2026-05-29 22:18:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
+    cloudflare(),
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     tailwindcss(),
